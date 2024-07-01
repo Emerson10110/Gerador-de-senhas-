d2d9ad0b447d18353a19fef9cf50ebdd7c579fc8
Emerson pinho <the.emerson.araujo@gmail.com>
2024-07-01 08:46:17 -0300

    Atualizar o codigo-do-gerador-de-senhas.py
    Na versão anterior não definimos a variavel senha, Agora adicionamos! :)

--- codigo-do-gerador-de-senhas.py
+++ codigo-do-gerador-de-senhas.py
@@ -6,23 +6,24 @@ import random
 # sorteamento  das letras
 
 letras = 'adDeEfFgGhHiIjJkKlLMmNnoOPpQqRrSsTtUuvVWwxXyYZzZ'
-letra_sorteada = random.sample(letras, 6)
+letra_sorteada = random.sample(letras, 7)
 resultado = ''.join(letra_sorteada)
 
 # sorteamento dos numeros 
 
 numeros = '1234567890'
-numeros_sorteado = random.sample(numeros, 4)
+numeros_sorteado = random.sample(numeros, 6 )
 resultadonumero = ''.join(numeros_sorteado)
 
 # sorteamento dos caracteres
 
 caracteres = '!@#$%^&*?~'
-caracteres_sorteado = random.sample(caracteres, 4)
+caracteres_sorteado = random.sample(caracteres, 5)
 resultado_caracteres = ''.join(caracteres_sorteado)
-print(resultado + resultadonumero + resultado_caracteres)
 
+#Criando a variavel senha
 
-
+senha = resultado + resultadonumero + resultado_caracteres
+print(senha)
 
 
